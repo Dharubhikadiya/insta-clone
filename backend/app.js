5ef4c530b81320b3f4405996e017e4cd28a985ff
Dharubhikadiya <bhikadiyadharmi55@gmail.com>
2025-01-06 22:16:05 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 const connectToDb = require("./db");
 const bcrypt = require("bcrypt");
